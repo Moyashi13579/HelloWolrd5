b60290065935f8b9cc56ebd1e4d9cbdce79078c0
Matsugawa_Rikiya <Matsugawa_Rikiya@NEW24WIN003>
2026-09-03 17:47:33 +0900

    プロジェクトファイルを追加。

---
+++ HelloWolrd5/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("あなたの国語の点数を入力してください：");
+int japaneseScore = Convert.ToInt32(Console.ReadLine());
+
+if (japaneseScore >= 80)
+{
+    Console.WriteLine("あなたは優秀です");
+}
+else if (japaneseScore >= 60)
+{
+    Console.WriteLine("まあまあまあ、いいんじゃない？");
+}
+else
+{
+    Console.WriteLine("もっと頑張りましょう");
+}
